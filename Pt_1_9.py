0586a306b6be3e0e5ad8351eeebae1551b46bf0e
Nikita <nikitosiartsev@gmail.com>
2023-07-05 16:06:45 +0700

    Pt_1_9: program receives two numbers from user and output message like "Произведение чисел 4 и 5 равно 20."

---
+++ Pt_1_9.py
@@ -0,0 +1,3 @@
+number_one = int(input("Введите число: "))
+number_two = int(input("Введите число: "))
+print("Произведение чисел {0} и {1} равно {number}.".format(number_one, number_two, number=number_one * number_two))
