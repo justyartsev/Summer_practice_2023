83ff5bda1407e4cad43c392e24c248fcbe804530
Nikita <nikitosiartsev@gmail.com>
2023-07-05 11:26:15 +0700

    Pt_1_3: programm receive three numbers from user then tell what number is maximum and shows them from largest to smallest.

---
+++ Pt_1_3.py
@@ -0,0 +1,8 @@
+first_number = int(input("Введите число: "))
+second_number = int(input("Введите число: "))
+third_number = int(input("Введите число: "))
+print("Наибольшее число:",
+      max(first_number, second_number, third_number))
+print(max(first_number, second_number, third_number), first_number + second_number + third_number
+      - max(first_number, second_number, third_number) - min(first_number, second_number, third_number)
+      , min(first_number, second_number, third_number))
