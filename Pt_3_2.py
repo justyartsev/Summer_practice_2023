3ce3c377e40d6fc44e29eed093456fecaa63f838
Nikita <nikitosiartsev@gmail.com>
2023-07-21 21:18:56 +0700

    Pt_3_2: fibonacci numbers.
    Lambda function that returns a list of all Fibonacci numbers
    up to a given number.
    Change: fix line length mistake.

--- Pt_3_2.py
+++ Pt_3_2.py
@@ -1,3 +1,4 @@
-fib_number = lambda number: number if number <= 1 else fib_number(number - 1) + fib_number(number - 2)
+fib_number = lambda number: number if number <= 1 else \
+    fib_number(number - 1) + fib_number(number - 2)
 fib_row = lambda y: [fib_number(i) for i in range(0, y)]
 print(fib_row(7))
