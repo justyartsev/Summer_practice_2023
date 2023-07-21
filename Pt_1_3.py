1aad6e4b69354bc6fdf1bf5befec3e6efe68a69d
Nikita <nikitosiartsev@gmail.com>
2023-07-21 14:33:57 +0700

    Pt_1_3: comparison of three numbers.
    Programm receive three numbers from user then tell what
    number is maximum and shows them from largest to smallest.
    Edit: fix line length mistake.

--- Pt_1_3.py
+++ Pt_1_3.py
@@ -3,6 +3,7 @@ second_number = int(input("Введите число: "))
 third_number = int(input("Введите число: "))
 print("Наибольшее число:",
       max(first_number, second_number, third_number))
-print(max(first_number, second_number, third_number), first_number + second_number + third_number
-      - max(first_number, second_number, third_number) - min(first_number, second_number, third_number)
-      , min(first_number, second_number, third_number))
+print(max(first_number, second_number, third_number),
+      first_number + second_number + third_number - max(first_number,
+      second_number, third_number) - min(first_number, second_number,
+      third_number), min(first_number, second_number, third_number))
