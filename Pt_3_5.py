a8783f6d648c23901f61584b4e8e43ad8e70373d
Nikita <nikitosiartsev@gmail.com>
2023-07-21 21:29:47 +0700

    Pt_3_5: program return list of numbers that are not prime.
    Change: fix line length mistake.

--- Pt_3_5.py
+++ Pt_3_5.py
@@ -1,3 +1,4 @@
 amount = int(input("Введите число: "))
-not_simple = [x for x in range(2, amount+1) if any(x % i == 0 for i in range(2, int(x ** 0.5) + 1))]
+not_simple = [x for x in range(2, amount+1) if
+              any(x % i == 0 for i in range(2, int(x ** 0.5) + 1))]
 print(not_simple)
