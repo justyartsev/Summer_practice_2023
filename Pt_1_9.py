defdb973b6038b005247fe3092565b65eef5147e
Nikita <nikitosiartsev@gmail.com>
2023-07-21 14:37:52 +0700

    Pt_1_9: multiplication of numbers.
    Program receives two numbers from user and output message
    like "Произведение чисел 4 и 5 равно 20."
    Change: fix line length mistake.

--- Pt_1_9.py
+++ Pt_1_9.py
@@ -1,3 +1,4 @@
 number_one = int(input("Введите число: "))
 number_two = int(input("Введите число: "))
-print("Произведение чисел {0} и {1} равно {number}.".format(number_one, number_two, number=number_one * number_two))
+print("Произведение чисел {0} и {1} равно {number}."
+      .format(number_one, number_two, number=number_one * number_two))
