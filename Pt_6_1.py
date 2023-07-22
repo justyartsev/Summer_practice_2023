40473cf49e3356485139a82e7d60091ba7a2999a
Nikita <nikitosiartsev@gmail.com>
2023-07-22 10:05:43 +0700

    Pt_6_1: parser of music of certain artist from Yandex Music.
    Change: fix line length mistake.

--- Pt_6_1.py
+++ Pt_6_1.py
@@ -8,10 +8,12 @@ while True:
         URL = requests.get(link)
         break
     except requests.exceptions.MissingSchema:
-        logger.error("Incorrect URL! Follow the example\nExample: https://music.yandex.ru/artist/XXXXXX")
+        logger.error("Incorrect URL! Follow the example\n"
+                     "Example: https://music.yandex.ru/artist/XXXXXX")
 
     except requests.exceptions.InvalidSchema:
-        logger.error("Use the correct scheme in the request URL.\nExample: https://music.yandex.ru/artist/XXXXXX")
+        logger.error("Use the correct scheme in the request URL.\n"
+                     "Example: https://music.yandex.ru/artist/XXXXXX")
 
     except requests.exceptions.RequestException as e:
         logger.error(e)
@@ -20,7 +22,8 @@ tracks_URL = link + "/tracks"
 page = requests.get(tracks_URL)
 soup = BeautifulSoup(page.text, "lxml")
 artist = soup.find("h1", class_="page-artist__title typo-h1 typo-h1_big").text
-tracks_info = soup.find_all("a", class_="d-track__title deco-link deco-link_stronger")
+tracks_info = soup.\
+    find_all("a", class_="d-track__title deco-link deco-link_stronger")
 print(f"10 самых популярных песен исполнителя - {artist}\n")
 for i in range(10):
     print(i + 1, "-", tracks_info[i].text.strip())
