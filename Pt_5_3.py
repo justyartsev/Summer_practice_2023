79ca0618626d1f5e01cadf2788801e9276a7e494
Nikita <nikitosiartsev@gmail.com>
2023-07-22 10:00:22 +0700

    Pt_5_3: finding books in certain range of years.
    Change: add newline at the end of the file.

--- Pt_5_3.py
+++ Pt_5_3.py
@@ -5,7 +5,8 @@ author_header = "Автор"
 release_year_header = "Год выпуска"
 with open("Book.csv", mode="w") as csv_file:
     headers = [book_header, author_header, release_year_header]
-    file_writer = csv.DictWriter(csv_file, delimiter=" ", lineterminator="\r", fieldnames=headers)
+    file_writer = csv.DictWriter(csv_file, delimiter=" ",
+                                 lineterminator="\r", fieldnames=headers)
     file_writer.writeheader()
     file_writer.writerow({book_header: "Мактуб", author_header: "Пауло Коэльо", release_year_header: 1994})
     file_writer.writerow({book_header: "Мирный воин", author_header: "Дэн Миллмэн", release_year_header: 1980})
